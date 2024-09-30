d867ff33c47aa957ef27b98d183e7d65f1599e36
esraabasher <esraabasher41@gmail.com>
2024-09-30 18:10:37 +0300

    testt

--- 01_create_experiment.py
+++ 01_create_experiment.py
@@ -3,8 +3,8 @@ import mlflow
 if __name__ == "__main__":
     # create a new mlflow experiment
     experiment_id = mlflow.create_experiment(
-        name="testing_mlflow1",
-        artifact_location="testing_mlflow1_artifacts",
+        name="test_mlflow1",
+        artifact_location="test_mlflow1_artifacts",
         tags={"env": "dev", "version": "1.0.0"},
     )
 
